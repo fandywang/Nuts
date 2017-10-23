f663692766694b3765401c7abb5e9404fc00f717
fandywang <ofandywang@gmail.com>
2017-10-23 20:00:29 +0800

    update utils

--- src/utils.py
+++ src/utils.py
@@ -11,20 +11,16 @@ import sys
 
 FLOAT_MAX = sys.float_info.max
 
+def run_wait_cmd(cmd):
+    logging.info('run cmd: %s' % cmd)
+    p = subprocess.Popen(cmd, shell=True, close_fds=True, env=os.environ)
+    ret = p.wait()
+    return ret
+
 def run_cmd(cmd):
     logging.info('run cmd: %s' % cmd)
     p = subprocess.Popen(cmd, shell=True, env=os.environ)
-    p.cmd = cmd
     return p
-
-def run_wait_cmd(cmd):
-    logging.info('run cmd: %s' % cmd)
-    p = subprocess.Popen(cmd, shell=True,
-            #stdout=subprocess.PIPE,
-            #stderr=subprocess.PIPE,
-            close_fds=True, env=os.environ)
-    ret = p.wait()
-    return ret
 
 def wait_cmd(p, timeout=FLOAT_MAX, refresh_interval=1.0):
     while True:
